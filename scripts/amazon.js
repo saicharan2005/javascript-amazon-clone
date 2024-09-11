66f1c778a5a60c41d3a807d2bcff35e8effec433
saicharan2005 <kundurusaicharan@gmail.com>
2024-09-11 23:07:43 +0530

    modified amazon.js

---
+++ scripts/amazon.js
@@ -0,0 +1,78 @@
+const products = [
+  {
+    img: "images/products/intermediate-composite-basketball.jpg",
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4,
+      count: 127,
+    },
+    priceCents: 2095,
+  },
+  {
+    img: "images/products/athletic-cotton-socks-6-pairs.jpg",
+    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+    rating: {
+      stars: 4.5,
+      count: 87,
+    },
+    priceCents: 1090,
+  },
+  {
+    img: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+    name: "Adults Plain Cotton T-Shirt - 2 Pack",
+    rating: {
+      stars: 4.5,
+      count: 56,
+    },
+    priceCents: 799,
+  },
+];
+
+
+let productsHtml=''
+products.forEach((product) => {
+  // const itemHtml =`
+ productsHtml+=`
+            <div class="each-product">
+                <div class="product-img-containter">
+                  <img class="product-img" src=${product.img}
+                  alt="intermediate-composite-basketball">
+                </div>
+                <div class="product-name">
+                  ${product.name}
+                </div>
+                <div class="product-rating-container">
+                  <img class="product-rating" src="images/ratings/rating-${
+                    product.rating.stars * 10
+                  }.png" alt="rating">
+                  <div class="rating-count">${product.rating.count}</div>
+                </div>
+                <div class="product-price">$${(product.priceCents)/100}</div>
+                <div class="product-quantity-container">
+
+                  <select >
+                    <option  selected value="1">1</option>
+                    <option   value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option   value="5">5</option>
+                    <option   value="6">6</option>
+                    <option  value="7">7</option>
+                    <option value="8">8</option>
+                    <option  value="9">9</option>
+                    <option   value="10">10</option>
+
+                  </select>
+                </div>
+                
+                  
+                  <button class="add-to-button button-p">Add to Cart</button>
+                
+
+
+              </div>
+          `;
+  
+});
+
+document.querySelector(".products-js").innerHTML = productsHtml;
