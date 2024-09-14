60d51d98efca644f15441937172c8fb87868cfc9
saicharan2005 <kundurusaicharan@gmail.com>
2024-09-14 21:10:11 +0530

    updated checkout

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,44 +1,45 @@
-const products = [
-  {
-    img: "images/products/intermediate-composite-basketball.jpg",
-    name: "Intermediate Size Basketball",
-    rating: {
-      stars: 4,
-      count: 127,
-    },
-    priceCents: 2095,
-  },
-  {
-    img: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 4.5,
-      count: 87,
-    },
-    priceCents: 1090,
-  },
-  {
-    img: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
-    rating: {
-      stars: 4.5,
-      count: 56,
-    },
-    priceCents: 799,
-  },
-];
+// const products = [
+//   {
+//     img: "images/products/intermediate-composite-basketball.jpg",
+//     name: "Intermediate Size Basketball",
+//     rating: {
+//       stars: 4,
+//       count: 127,
+//     },
+//     priceCents: 2095,
+//   },
+//   {
+//     img: "images/products/athletic-cotton-socks-6-pairs.jpg",
+//     name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+//     rating: {
+//       stars: 4.5,
+//       count: 87,
+//     },
+//     priceCents: 1090,
+//   },
+//   {
+//     img: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+//     name: "Adults Plain Cotton T-Shirt - 2 Pack",
+//     rating: {
+//       stars: 4.5,
+//       count: 56,
+//     },
+//     priceCents: 799,
+//   },
+// ];
 
+// the above code we  already have data file we  can add it
 
-let productsHtml=''
+let productsHtml = "";
 products.forEach((product) => {
   // const itemHtml =`
- productsHtml+=`
+  productsHtml += `
             <div class="each-product">
                 <div class="product-img-containter">
-                  <img class="product-img" src=${product.img}
+                  <img class="product-img" src=${product.image}
                   alt="intermediate-composite-basketball">
                 </div>
-                <div class="product-name">
+                <div class="product-name limit-text-to-2-lines">
                   ${product.name}
                 </div>
                 <div class="product-rating-container">
@@ -47,8 +48,11 @@ products.forEach((product) => {
                   }.png" alt="rating">
                   <div class="rating-count">${product.rating.count}</div>
                 </div>
-                <div class="product-price">$${(product.priceCents)/100}</div>
+                <div class="product-price">$${(
+                  product.priceCents / 100
+                ).toFixed(2)}</div>
                 <div class="product-quantity-container">
+
 
                   <select >
                     <option  selected value="1">1</option>
@@ -64,6 +68,10 @@ products.forEach((product) => {
 
                   </select>
                 </div>
+
+                <div class="product-spacer"></div>
+                  <div class="add-to-cart"> 
+                    <img src="images/icons/checkmark.png" alt="checkmark">Added</div>
                 
                   
                   <button class="add-to-button button-p">Add to Cart</button>
@@ -72,7 +80,10 @@ products.forEach((product) => {
 
               </div>
           `;
-  
 });
 
 document.querySelector(".products-js").innerHTML = productsHtml;
+
+
+
+
